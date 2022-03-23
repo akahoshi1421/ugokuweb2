a0b3e96c761d6349c82d870806b63e7a44781606
akahoshi1421 <akaho1421@gmail.com>
2022-03-24 01:22:09 +0900

    3/23

--- page-top/js/pgtop7.js
+++ page-top/js/pgtop7.js
@@ -1,39 +1,35 @@
 //スクロールした際の動きを関数でまとめる
-function PageTopAnime() {
-	var scroll = $(window).scrollTop();
-	if (scroll >= 100){//上から100pxスクロールしたら
-		$('#page-top').removeClass('DownMove');//#page-topについているDownMoveというクラス名を除く
-		$('#page-top').addClass('UpMove');//#page-topについているUpMoveというクラス名を付与
-	}else{
-		if($('#page-top').hasClass('UpMove')){//すでに#page-topにUpMoveというクラス名がついていたら
-			$('#page-top').removeClass('UpMove');//UpMoveというクラス名を除き
-			$('#page-top').addClass('DownMove');//DownMoveというクラス名を#page-topに付与
-		}
+function PageTopCheck(){
+    var winScrollTop = $(this).scrollTop();
+    var secondTop =  $("#area-2").offset().top - 150; //#area-2の上から150pxの位置まで来たら
+    if(winScrollTop >= secondTop){
+		$('.js-scroll').removeClass('scroll-view');//.js-scrollからscroll-viewというクラス名を除去
+		$('.js-pagetop').addClass('scroll-view');//.js-pagetopにscroll-viewというクラス名を付与
+	} else {//元に戻ったら
+		$('.js-scroll').addClass('scroll-view');//.js-scrollからscroll-viewというクラス名を付与
+		$('.js-pagetop').removeClass('scroll-view');//.js-pagetopからscroll-viewというクラス名を除去
 	}
+
 }
 
-// 画面をスクロールをしたら動かしたい場合の記述
-$(window).scroll(function () {
-	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-});
-
-// ページが読み込まれたらすぐに動かしたい場合の記述
-$(window).on('load', function () {
-	PageTopAnime();/* スクロールした際の動きの関数を呼ぶ*/
-});
-
-
-// #page-topをクリックした際の設定
-$('#page-top').click(function () {
-	var scroll = $(window).scrollTop(); //スクロール値を取得
-	if(scroll > 0){
-		$(this).addClass('floatAnime');//クリックしたらfloatAnimeというクラス名が付与
-        $('body,html').animate({
-            scrollTop: 0
-        }, 2000,function(){//スクロールの速さ。数字が大きくなるほど遅くなる
-            $('#page-top').removeClass('floatAnime');//上までスクロールしたらfloatAnimeというクラス名を除く
-        });	
+//クリックした際の動き
+$('.scroll-top a').click(function () {
+	var elmHash = $(this).attr('href'); //hrefの内容を取得
+	if (elmHash == "#area-2") {//もし、リンク先のhref の後が#area-2の場合
+		var pos = $(elmHash).offset().top;
+		$('body,html').animate({scrollTop: pos}, pos); //#area-2にスクロール
+	}else{
+		$('body,html').animate({scrollTop: 0}, 500); //それ以外はトップへスクロール。数字が大きくなるほどゆっくりスクロール
 	}
     return false;//リンク自体の無効化
 });
 
+// 画面をスクロールをしたら動かしたい場合の記述
+$(window).scroll(function () {
+	PageTopCheck();/* スクロールした際の動きの関数を呼ぶ*/
+});
+
+// ページが読み込まれたらすぐに動かしたい場合の記述
+$(window).on('load', function () {
+	PageTopCheck();/* スクロールした際の動きの関数を呼ぶ*/
+});
